bf05b3a18fec28f5915e11c688ae2813458019c7
chltmdgh522 <chltmdgh517@naver.com>
2025-11-17 21:33:00 +0900

    [Fix]: 코틀린 초기 변환

---
+++ src/main/kotlin/historywowa/HistorywowaApplication.kt
@@ -0,0 +1,11 @@
+package historywowa.historywowa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HistorywowaApplication
+
+fun main(args: Array<String>) {
+	runApplication<HistorywowaApplication>(*args)
+}
